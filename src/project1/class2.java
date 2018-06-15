6008fc627b7c445bfb149aebcaf0b00f307ba389
agnesdonat <agi.donat@gmail.com>
2018-06-15 17:23:50 +0100

    Minimum values for int, long, short, byte

--- src/project1/class2.java
+++ src/project1/class2.java
@@ -6,6 +6,7 @@ public class class2 {
 		// 'int' is 32 bits, it can hold the maximum value of 2147483647
 		int a = 1;
 		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
         System.out.println(a);
         
         // When we need to assign a number that is larger than 2147483647 we use 'long'
@@ -13,16 +14,19 @@ public class class2 {
         // We need to specify the data type adding 'L' to the end
         long b = 2147483648L;
         System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
         System.out.println(b);
         
         // 'short' holds less than an 'int': 32767
         short c = 12;
         System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
         System.out.println(c);
         
         // 'byte' is the last integer data type and it has an even smaller maximum value of 127 
         byte d = 126;
         System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
         System.out.println(d);
 	}
 
