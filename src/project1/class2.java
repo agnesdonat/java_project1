ddd134e8616397a43e6fd8ff14091c55408e61f4
agnesdonat <agi.donat@gmail.com>
2018-06-15 16:57:24 +0100

    Int

--- src/project1/class2.java
+++ src/project1/class2.java
@@ -3,8 +3,10 @@ package project1;
 public class class2 {
 
 	public static void main(String[] args) {
+		// 'int' is 32 bits, it can hold the maximum value of 2147483647
 		int a = 1;
-
+		System.out.println(Integer.MAX_VALUE);
+        System.out.println(a);
 	}
 
 }
