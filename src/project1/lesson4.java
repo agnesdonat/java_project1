dd1632154cb5050ac2388f3aa0e2f57693e29635
agnesdonat <agi.donat@gmail.com>
2018-06-18 17:09:14 +0100

    Booleans

---
+++ src/project1/lesson4.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class lesson4 {
+
+	public static void main(String[] args) {
+		
+		boolean a = true;
+		boolean b = false;
+		boolean c = true;
+		
+		if (a && b && c) {
+			System.out.println("Hello, World!");
+		}
+
+	}
+
+}
