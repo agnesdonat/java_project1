3473eff87bf3bea1f45edce1acce08408f6f527d
agnesdonat <agi.donat@gmail.com>
2018-06-20 17:44:52 +0100

    Refactor

--- src/project1/lesson6.java
+++ src/project1/lesson6.java
@@ -51,8 +51,17 @@ public class lesson6 {
 		d = "hows it going";
 		e = "whats up";
 				
-	    String result = "" + a.charAt(0) + b.charAt(0) + c.charAt(0) + d.charAt(0) + e.charAt(0)
-	     + a.charAt(a.length() - 1) + b.charAt(b.length() - 1) + c.charAt(c.length() - 1) + d.charAt(d.length() - 1) + e.charAt(e.length() - 1);
+	    String result = "" + a.charAt(0);
+	    result += b.charAt(0);
+	    result += c.charAt(0);
+	    result += d.charAt(0);
+	    result += e.charAt(0);
+	    
+	    result += a.charAt(a.length() - 1);
+	    result += b.charAt(b.length() - 1);
+	    result += c.charAt(c.length() - 1);
+	    result += d.charAt(d.length() - 1);
+	    result += e.charAt(e.length() - 1);
 	    
 	    System.out.println(result);
 	}
