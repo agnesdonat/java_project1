04bfe34e1b98f4b97f1787e5bb776c4b0567bcd5
agnesdonat <agi.donat@gmail.com>
2018-06-21 16:24:30 +0100

    Switch statemnts

---
+++ src/project1/lesson8.java
@@ -0,0 +1,48 @@
+package project1;
+
+public class lesson8 {
+
+	public static void main(String[] args) {
+		// Check whether or not the number is divisible by 10 using Switch statements
+		
+		int a = 87;
+		
+		switch(a) {
+		case 10:
+			System.out.println("a is divisible by 10");
+			break;
+		case 20:
+			System.out.println("a is divisible by 10");
+			break;
+		case 30:
+			System.out.println("a is divisible by 10");
+			break;
+		case 40:
+			System.out.println("a is divisible by 10");
+			break;
+		case 50:
+			System.out.println("a is divisible by 10");
+			break;
+		case 60:
+			System.out.println("a is divisible by 10");
+			break;
+		case 70:
+			System.out.println("a is divisible by 10");
+			break;
+		case 80:
+			System.out.println("a is divisible by 10");
+			break;
+		case 90:
+			System.out.println("a is divisible by 10");
+			break;
+		case 100:
+			System.out.println("a is divisible by 10");
+			break;
+		default:
+			System.out.println("a is either under 100 or not divisible by 10");
+			break;	
+		}
+
+	}
+
+}
