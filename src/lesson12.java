367f9eab65dfe54ede47f845ebf1192215e0d5cd
agnesdonat <agi.donat@gmail.com>
2018-06-27 18:25:27 +0100

    Exercise set up

---
+++ src/lesson12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lesson12 {
+
+	public static void main(String[] args) {
+		
+		// Add multiplication and division
+		// Without restarting the program the user can perform more operations
+		// Add a break statement to end the application
+		
+		Scanner r = new Scanner(System.in);		
+		
+		while(true) {
+			System.out.println(r.nextLine());
+		} 
+
+	}
+
+}
